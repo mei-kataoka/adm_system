6c8d1bfee57c105c4c259fc360eeacd46a034dc9
mei-kataoka <mk.13.6tones@gmail.com>
2023-04-10 13:59:54 +0900

    main

--- public/js/road.js
+++ public/js/road.js
@@ -18,12 +18,38 @@ $(document).on('click', '#searchBtn', function () {
 
     }).done(function (data) {//通信が成功したばあい
         $('.table-row').empty();
+        $('.loading').addClass('display-none');
         console.log('ok');
         console.log(data);
         let html = 'test';
 
         $.each(data, function (index, value) {
             console.log(index + ': ' + value);
+            /**
+          let id = value.id;
+          let company_id = value.product_name;
+          let price = value.price;
+          let stock = value.stock;
+          let img_path = value.img_path;
+
+          html = `
+             
+  <tr class='table-row'>
+      <td>${id}</td>
+      <td><img src="{{asset('storage/${img_path}')}}" style=" max-width: 100%;" alt=""></td>
+      <td>${product_name}</td>
+      <td>${price}</td>
+      <td>${stock}</td>
+      <td>${company_id}</td>
+      <td><a href="/adm/public/product/${id} ">詳細</a></td>
+      <td><button type="button" class="btn btn-primary" onclick="location.href='/adm/public/product/edit/${id} '">編集</button></td>
+      <form method="POST" action="{{ route('delete',${id} " onSubmit="return checkDelete()" enctype="multipart/form-data">
+          @csrf
+          <td><button type="submit" class="btn btn-primary" onclick="">削除</button></td>
+  </tr>
+       `;
+          $('.user-table').append(html);
+           */
         });
 
     }).fail(function (jqXHR, textStatus, errorThrown) { // 通信が失敗したときの処理
